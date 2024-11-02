df63eb00beb11bd97a8dfd6781ad0adc4093ff11
Jude Francia <65160232+jafrnq@users.noreply.github.com>
2024-11-02 16:09:04 +0800

    added file manipulation functionality

--- RunThis.py
+++ RunThis.py
@@ -1,6 +1,10 @@
 import datetime;
 
 date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S");
+finalOutputString = print("Test Successfully ran at:" + date)
 
-print("Hello World")
-print("Test Successfully ran at:" + date)
+#open a text file
+with open("testResults.txt", "w") as file:
+    file.write(finalOutputString)
+
+print("String has been written to output.txt")
